rework rewards units, fix port problem, fix rollout unity glitch

pythonSide/UnitySingleCarEnv.py
@@ -26,6 +26,16 @@ def wait_for_port(host: str, port: int, timeout=20):
     print(f"Timeout: Unity did not open port {port} in {timeout} seconds.")
     return False
 
+def is_port_free(port: int) -> bool:
+    """Check if a TCP port is available."""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        try:
+            s.bind(("127.0.0.1", port))
+        except OSError:
+            return False
+    return True
+
 
 class UnityCarEnv(gym.Env):
     """Unity Car environment for PPO training and inference."""
@@ -93,11 +103,27 @@ class UnityCarEnv(gym.Env):
         self.frame_buffer[-1] = new_frame
 
     def _launch_unity(self):
-        return #uncomment for manual unity launch
+        #return #uncomment for manual unity launch, then default ports are expected
         """Launch Unity executable with port arguments."""
         if not os.path.exists(self.unity_exe_path):
             raise FileNotFoundError(f"Unity executable not found: {self.unity_exe_path}")
 
+            # --- PORT VALIDATION LOOP ---
+        while True:
+            free_control = is_port_free(self.control_port)
+            free_car_instr = is_port_free(self.car_instr_port)
+            free_obs = is_port_free(self.obs_port)
+ 
+            if free_control and free_car_instr and free_obs:
+                break  # all good
+
+            print(f"Ports [{self.control_port}, {self.car_instr_port}, {self.obs_port}] "
+                f"in use. Shifting +3 and retrying...")
+
+            self.control_port += 3
+            self.car_instr_port += 3
+            self.obs_port += 3
+            
         # Pass ports as command-line args
         args = [
             self.unity_exe_path,
@@ -195,19 +221,28 @@ class UnityCarEnv(gym.Env):
         # --- Update state ---
         self.prev_action = np.clip(action, -1.0, 1.0)
         self.current_step += 1
-        self.episode_reward += reward
 
         truncated = False
         done = self.current_step >= self.max_steps
 
         # crashed
-        if obs_packet.rewards.collision_penalty == -1:
+        if obs_packet.rewards.collision_penalty > 0.5:
             truncated = True
             done = True
             
-        if obs_packet.rewards.out_of_bounds_penalty == -1:
+        if obs_packet.rewards.out_of_bounds_penalty > 0.5:
             truncated = True
             done = True
+            self.episode_reward -= reward
+            reward = 0
+            
+        if obs_packet.rewards.acceleration > 100:
+            truncated = True
+            done = True
+            self.episode_reward -= reward
+            reward = 0
+            
+        self.episode_reward += reward
 
         info = {}
         if done:

pythonSide/rewards.py
@@ -62,13 +62,19 @@ class Rewards:
 
             self.out_of_bounds_penalty,
             self.collision_penalty,
+            self.discounted_collision_penalty,
             self.grass_penalty,
+            self.discounted_grass_penalty,
 
             self.team_distance,
             self.lap_time,
+            self.discounted_lap_time,
             self.team_lap_time,
+            self.discounted_team_lap_time,
             self.placement,
+            self.discounted_placement,
             self.team_placement,
+            self.discounted_team_placement,
 
             self.speed,
             self.acceleration,
@@ -97,9 +103,9 @@ class Rewards:
             self.distanceII,
             self.distanceIII,
             self.progressReward,
+            self.discounted_progressReward,
 
             # reserved for later use
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
         ], dtype=np.float32)
 
@@ -114,9 +120,9 @@ class Rewards:
 
             out_of_bounds_penalty = -0.0,
             collision_penalty = -0.0,
-            discounted_collision_penalty= -0.1,
-            grass_penalty = -0.0,
-            discounted_grass_penalty= -0.01,
+            discounted_collision_penalty= -0.0,
+            grass_penalty = -0.1,
+            discounted_grass_penalty= -0.0,
 
             team_distance = -0.0,
             lap_time = -0.0,
@@ -131,31 +137,31 @@ class Rewards:
             speed = 0.0,
             acceleration = 0.0,
             distance = 0.0,
-            discounted_distance = 0.01,
+            discounted_distance = 0.,
 
             speedI = 0.0,
             speedII = 0.0,
             speedIII = 0.0,
             speedIV = 0.0,
-            speedV = 0.02,
+            speedV = 0.01,
 
             accelerationI = 0.0,
             accelerationII = 0.0,
             accelerationIII = 0.0,
-            accelerationIV = 0.01,
+            accelerationIV = 0.0,
             accelerationV = 0.0,
 
             angleI = -0.0,
             angleII = -0.0,
             angleIII = -0.0,
             angleIV = -0.0,
-            angleV = -0.02,
+            angleV = -0.2,
 
             distanceI = -0.0,
             distanceII = -0.0,
-            distanceIII = -0.1,
+            distanceIII = -0.0,
             progressReward = 0.0,
-            discounted_progressReward= 0.1
+            discounted_progressReward= 0.0
         )
 
 